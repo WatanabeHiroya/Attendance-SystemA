c7298cf57252ce9fb2be4a176fb4dfd6b357b300
hiroya <plastic.gu@gmail.com>
2020-01-12 02:57:39 +0000

    ユーザー編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :update_user_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :update_user_info]
-  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info, :index, :update_user_info]
   before_action :set_one_month, only: :show
-  before_action :admin_or_correct_user, only: [:show]
+  before_action :admin_or_correct_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -42,7 +41,7 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
-      redirect_to @user
+      redirect_to root_url
     else
       render :edit      
     end
@@ -53,7 +52,7 @@ class UsersController < ApplicationController
       flash[:success] = "ユーザー情報を編集しました。"
       redirect_to users_url
     else
-      render :index
+      render :edit
     end
   end
 
