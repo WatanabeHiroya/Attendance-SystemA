f59c4b1a08eb0f9e13929187fe8fbdd65df459a6
hiroya <plastic.gu@gmail.com>
2020-01-19 06:28:22 +0000

    拠点情報削除機能実装中

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -5,22 +5,36 @@ class BasesController < ApplicationController
   end
   
   def edit
+    @base = Base.find(params[:id])
   end
   
   def update
+    @base = Base.find(params[:id])
+    if @base.update_attributes(base_params)
+      flash[:success] = "拠点情報を修正しました。"
+      redirect_to bases_url
+    else
+      render :edit
+    end
   end
   
   def destroy
+    @base = Base.find(params[:id])
+    if @base.destroy
+      flash[:success] = "拠点情報を削除しました。"
+      redirect_to bases_url
+    end
   end
   
   def new
+    @base = Base.new
   end
   
   def create
-    @base = Base.find(params[base_params])
+    @base = Base.new(base_params)
     if @base.save
-      flash[:success] = ""
-      redirect_to bases_show_url
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to bases_url
     else
       render :new
     end
@@ -29,6 +43,6 @@ class BasesController < ApplicationController
   private
   
     def base_params
-      params.require(:base).parmit(:base_number, :base_name, :attendance_type)
+      params.require(:base).permit(:base_number, :base_name, :attendance_type)
     end
 end
