54f57f66e578f2b20f9b4cf44554f7d969e8d26e
hiroya <plastic.gu@gmail.com>
2020-04-16 21:37:44 +0900

    チェックボックス編集中

--- db/migrate/20200204115725_add_status_to_attendances.rb
+++ db/migrate/20200204115725_add_status_to_attendances.rb
@@ -1,5 +1,6 @@
 class AddStatusToAttendances < ActiveRecord::Migration[5.1]
   def change
     add_column :attendances, :status, :string
+    add_column :attendances, :change_check_box, :string
   end
 end
--- db/migrate/20200321061924_add_affiliations_to_attendances.rb
+++ db/migrate/20200321061924_add_affiliations_to_attendances.rb
@@ -2,5 +2,6 @@ class AddAffiliationsToAttendances < ActiveRecord::Migration[5.1]
   def change
     add_column :attendances, :affiliation_status, :string
     add_column :attendances, :affiliation_instruction, :string
+    add_column :attendances, :affiliation_change_check_box, :string
   end
 end
