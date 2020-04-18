54a777be80c4c588407ae168db7690728416ed66
hiroya <plastic.gu@gmail.com>
2020-04-18 21:52:31 +0900

    指定勤務終了時間より早い時間は申請できない様にする

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,6 +7,7 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  validate :apply_overtime
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -19,5 +20,8 @@ class Attendance < ApplicationRecord
       end
     end
   end
-
+  
+  def apply_overtime
+    errors.add(:designated_work_end_time, "より早い終了予定時間は無効です。") if designated_work_end_time > overtime_finished_at
+  end
 end
