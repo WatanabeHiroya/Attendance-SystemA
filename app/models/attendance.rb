4fd26fac2c31c8df650c1abdac5cc27f44f7f750
hiroya <plastic.gu@gmail.com>
2020-04-18 20:41:05 +0900

    残業申請 指示者確認修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,7 +7,6 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
-  validate :edit_attendances
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -20,10 +19,5 @@ class Attendance < ApplicationRecord
       end
     end
   end
-  
-  def edit_attendances
-    if started_at.present? && instruction.nil?
-      errors.add(:instruction, "を選択してください。")
-    end
-  end
+
 end
