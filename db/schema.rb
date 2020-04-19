2c92f9a2c4575b9ddc3fce207444a178182d1951
hiroya <plastic.gu@gmail.com>
2020-04-19 14:37:48 +0900

    当日以降の残業申請禁止

--- db/schema.rb
+++ db/schema.rb
@@ -55,14 +55,14 @@ ActiveRecord::Schema.define(version: 20200418091443) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-04-17 23:00:00"
-    t.datetime "work_time", default: "2020-04-17 22:30:00"
+    t.datetime "basic_time", default: "2020-04-18 23:00:00"
+    t.datetime "work_time", default: "2020-04-18 22:30:00"
     t.integer "employee_number"
     t.string "affiliation"
     t.string "uid"
-    t.datetime "basic_work_time", default: "2020-04-17 23:00:00"
-    t.datetime "designated_work_start_time", default: "2020-04-18 00:00:00"
-    t.datetime "designated_work_end_time", default: "2020-04-18 09:00:00"
+    t.datetime "basic_work_time", default: "2020-04-18 23:00:00"
+    t.datetime "designated_work_start_time", default: "2020-04-19 00:00:00"
+    t.datetime "designated_work_end_time", default: "2020-04-19 09:00:00"
     t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
